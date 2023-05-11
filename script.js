5f6cffed918405263fa5efe2d61a8142e2cb6bd5
bruno de clercq <brunojl.declercq@gmail.com>
2023-05-11 11:06:31 +0200

    leetcode

--- script.js
+++ script.js
@@ -93,59 +93,130 @@ consecutive strings : follow one after another without an interruption
 //   )
 // );
 
-document.querySelector("h2").insertAdjacentHTML(
+///////////// Codewars - So many permutations ////////////////////////////////////////////////////////
+// document.querySelector("h2").insertAdjacentHTML(
+//   "afterbegin",
+//   `<p>In this kata, your task is to create all permutations of a non-empty input string and remove duplicates, if present create as many "shufflings" as you can!</p>
+
+// <ul><p>Examples:</p>
+
+// <li>With input 'a':
+// Your function should return: ['a']</li>
+
+// <li>With input 'ab':
+// Your function should return ['ab', 'ba']</li>
+
+// <li>With input 'abc':
+// Your function should return ['abc','acb','bac','bca','cab','cba']</li>
+
+// <li>With input 'aabb':
+// Your function should return ['aabb', 'abab', 'abba', 'baab', 'baba', 'bbaa']</li></ul>
+
+// <p>Note: The order of the permutations doesn't matter.</p>
+
+// <p>Good luck!</p>`
+// );
+
+// function fac(num) {
+//   if (num < 0) return -1;
+//   else if (num === 0) return 1;
+//   else {
+//     return num * fac(num - 1);
+//   }
+// }
+
+// function calcUnique(string) {
+//   const obj = {};
+//   for (const el of [...string]) {
+//     obj[el] = (obj[el] || 0) + 1;
+//   }
+//   return [...Object.values(obj)];
+// }
+
+// function calcPerm(string) {
+//   const perm =
+//     fac(string.length) /
+//     calcUnique(string)
+//       .map((el) => fac(el))
+//       .reduce((acc, cur) => acc * cur, 1);
+//   return perm;
+// }
+
+// function permutator(string) {
+//   const arr = [];
+//   for (let i = 0; i < string.length; i++) {
+//     arr.push(string[i]);
+//   }
+//   return arr;
+// }
+// console.log(permutator("abcd"));
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+////////////////////////// Leetcode - 2629. Function Composition //////////////////////////
+document.querySelector(".assignment").insertAdjacentHTML(
   "afterbegin",
-  `<p>In this kata, your task is to create all permutations of a non-empty input string and remove duplicates, if present create as many "shufflings" as you can!</p>
+  `<div><p>Given an array of functions&nbsp;<code>[f<span style="font-size: 10.8333px;">1</span>, f<sub>2</sub>, f<sub>3</sub>,&nbsp;..., f<sub>n</sub>]</code>, return&nbsp;a new function&nbsp;<code>fn</code>&nbsp;that is the <strong>function&nbsp;composition</strong> of the array of functions.</p>
 
-<ul><p>Examples:</p>
+<p>The&nbsp;<strong>function&nbsp;composition</strong>&nbsp;of&nbsp;<code>[f(x), g(x), h(x)]</code>&nbsp;is&nbsp;<code>fn(x) = f(g(h(x)))</code>.</p>
 
-<li>With input 'a':
-Your function should return: ['a']</li>
+<p>The&nbsp;<strong>function&nbsp;composition</strong>&nbsp;of an empty list of functions is the&nbsp;<strong>identity function</strong>&nbsp;<code>f(x) = x</code>.</p>
 
-<li>With input 'ab':
-Your function should return ['ab', 'ba']</li>
+<p>You may assume each&nbsp;function&nbsp;in the array accepts one integer as input&nbsp;and returns one integer as output.</p>
 
-<li>With input 'abc':
-Your function should return ['abc','acb','bac','bca','cab','cba']</li>
+<p>&nbsp;</p>
+<p><strong class="example">Example 1:</strong></p>
 
-<li>With input 'aabb':
-Your function should return ['aabb', 'abab', 'abba', 'baab', 'baba', 'bbaa']</li></ul>
+<pre><strong>Input:</strong> functions = [x =&gt; x + 1, x =&gt; x * x, x =&gt; 2 * x], x = 4
+<strong>Output:</strong> 65
+<strong>Explanation:</strong>
+Evaluating from right to left ...
+Starting with x = 4.
+2 * (4) = 8
+(8) * (8) = 64
+(64) + 1 = 65
+</pre>
 
-<p>Note: The order of the permutations doesn't matter.</p>
+<p><strong class="example">Example 2:</strong></p>
 
-<p>Good luck!</p>`
+<pre><strong>Input:</strong> functions = [x =&gt; 10 * x, x =&gt; 10 * x, x =&gt; 10 * x], x = 1
+<strong>Output:</strong> 1000
+<strong>Explanation:</strong>
+Evaluating from right to left ...
+10 * (1) = 10
+10 * (10) = 100
+10 * (100) = 1000
+</pre>
+
+<p><strong class="example">Example 3:</strong></p>
+
+<pre><strong>Input:</strong> functions = [], x = 42
+<strong>Output:</strong> 42
+<strong>Explanation:</strong>
+The composition of zero functions is the identity function</pre>
+
+<p>&nbsp;</p>
+<p><strong>Constraints:</strong></p>
+
+<ul>
+	<li><code><font face="monospace">-1000 &lt;= x &lt;= 1000</font></code></li>
+	<li><code><font face="monospace">0 &lt;= functions.length &lt;= 1000</font></code></li>
+	<li><font face="monospace"><code>all functions accept and return a single integer</code></font></li>
+</ul>
+</div>`
 );
 
-function fac(num) {
-  if (num < 0) return -1;
-  else if (num === 0) return 1;
-  else {
-    return num * fac(num - 1);
-  }
-}
+const compose = function (functions) {
+  let c = -1;
+  return function calc(x) {
+    c++;
+    if (c === functions.length) {
+      return x;
+    } else {
+      return functions[c](calc(x));
+    }
+  };
+};
 
-function calcUnique(string) {
-  const obj = {};
-  for (const el of [...string]) {
-    obj[el] = (obj[el] || 0) + 1;
-  }
-  return [...Object.values(obj)];
-}
-
-function calcPerm(string) {
-  const perm =
-    fac(string.length) /
-    calcUnique(string)
-      .map((el) => fac(el))
-      .reduce((acc, cur) => acc * cur, 1);
-  return perm;
-}
-
-function permutator(string) {
-  const arr = [];
-  for (let i = 0; i < string.length; i++) {
-    arr.push(string[i]);
-  }
-  return arr;
-}
-console.log(permutator("abcd"));
+const fn = compose([]);
+console.log(fn(42));
